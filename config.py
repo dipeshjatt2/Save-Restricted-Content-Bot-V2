b96cfd2ca368f17988a9917657d0e09ff51cdda5
Kaminanoone <oberoielvish84@gmail.com>
2024-08-24 18:57:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int(getenv("API_ID", "29640188"))
+API_HASH = getenv("API_HASH", "e470abc84a3bc445997ee4ea5be87deb")
+BOT_TOKEN = getenv("BOT_TOKEN", "7285939082:AAGO0kX2-dY7yUBqUUSp6P8EDhz_d_IuYdo")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7504263874").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://syangshibo1:mongodbdatabase500@cluster0.vtkrte7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "4563305914")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "1002154901949"))
